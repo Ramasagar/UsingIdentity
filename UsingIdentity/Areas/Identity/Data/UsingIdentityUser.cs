87de1333fb10d5f32685bf0d9620ca2fc83c9117
Rama Sagar <sagarpulidindi@outlook.com>
2020-08-11 14:43:54 +0530

    Initial commit

---
+++ UsingIdentity/Areas/Identity/Data/UsingIdentityUser.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+
+namespace UsingIdentity.Areas.Identity.Data
+{
+    // Add profile data for application users by adding properties to the UsingIdentityUser class
+    public class UsingIdentityUser : IdentityUser
+    {
+        [PersonalData]
+        [Column(TypeName ="nvarchar(100)")]
+        public string Firstname { get; set; }
+        [PersonalData]
+        [Column(TypeName = "nvarchar(100)")]
+        public string LastName { get; set; }
+    }
+}
---
+++ UsingIdentity/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using UsingIdentity.Areas.Identity.Data;
+using UsingIdentity.Data;
+
+[assembly: HostingStartup(typeof(UsingIdentity.Areas.Identity.IdentityHostingStartup))]
+namespace UsingIdentity.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<UsingIdentityContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("UsingIdentityContextConnection")));
+
+                services.AddDefaultIdentity<UsingIdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                    .AddEntityFrameworkStores<UsingIdentityContext>();
+            });
+        }
+    }
+}
